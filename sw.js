50edaf100062ffd68a978b9f604fe76d90a8cf44
Ip pc17 <ippc17@policianacional.net>
2026-03-20 17:53:22 -0600

    Corrige sw.js para evitar 404

--- sw.js
+++ sw.js
@@ -1,26 +1,19 @@
 const CACHE_NAME = 'control-negocio-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/data.js'
+  '/index.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
